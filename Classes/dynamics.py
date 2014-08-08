Correcting bug in derivative calculation (not yet corrected)

Classes/dynamics.py
@@ -86,7 +86,7 @@ class Dynamics:
     def set_structure(self, structure):
         self._structure = structure
 
-    def get_velocity_mass_average(self,):
+    def get_velocity_mass_average(self):
 
         if self._velocity_mass_average is None:
             self._velocity_mass_average = np.empty_like(self.velocity)

main.py
@@ -7,6 +7,7 @@ import Functions.peaksearch as peaksearch
 import Functions.correlate as correlate
 import phonopy.file_IO as file_IO
 import Functions.phonopy_interface as pho_interface
+import Functions.energy as enerfunc
 
 print("Program start")
 
@@ -17,11 +18,11 @@ if True:
     #Parameters definition section (one parameter left)
 #    q_vector = np.array ([1.149468,1.149468,1.149468])
 #    q_vector = np.array ([1.149468/2,1.149468/2,1.149468/2])
-    q_vector_norm = np.array ([0.0,0.0,0.5])
+    q_vector_norm = np.array ([0.0, 0.0, 0.0])
 
 
 #    directory ='/home/abel/VASP/Bi2O3_phonon/'
-    directory ='/home/abel/VASP/Si-test_petit/'
+    directory ='/home/abel/VASP/Si-phonon/2x2x2/'
 
     #Reading structure
     structure = reading.read_from_file_structure(directory+'OUTCAR')
@@ -45,9 +46,10 @@ if True:
     print('primitive cell')
     print(structure.get_primitive_cell())
 
-#    q_vector = np.array(q_vector2 * 1.149468*2) #For cubic cell
-    q_vector = np.array(q_vector_norm * 2*np.pi/structure.get_primitive_cell()[0,0]) #For cubic cell
-
+#    q_vector = np.array(q_vector_norm * 1.149468*2) #For cubic cell
+#    q_vector = np.array(q_vector_norm * 2*np.pi/structure.get_primitive_cell()[0,0]) #For cubic cell
+    q_vector = np.prod([q_vector_norm,2*np.pi/structure.get_primitive_cell().diagonal()],axis=0)
+    print'q_vector:',q_vector_norm,q_vector
 
     #Reading force constants from vasprun.xml
     #force_constants = get_force_constants_from_file('/home/abel/VASP/Si-test/FORCE_CONSTANTS')
@@ -56,11 +58,12 @@ if True:
 
     #Getting eigenvectors from somewhere
     eigenvectors, original_frequencies = pho_interface.obtain_eigenvectors_from_phonopy(structure,q_vector_norm)
-
+    print(eigenvectors[4,:,:])
+    print(eigenvectors[5,:,:])
     #Reading trajectory from test files
  #   trajectory = reading.read_from_file_trajectory('/home/abel/VASP/Bi2O3_md/OUTCAR',structure)
- #   trajectory = reading.read_from_file_trajectory('/home/abel/VASP/Si-dynamic_600/RUN5B/OUTCAR',structure)
-    trajectory = reading.generate_test_trajectory(structure,eigenvectors,original_frequencies,q_vector)
+    trajectory = reading.read_from_file_trajectory('/home/abel/VASP/Si-dynamic_600/RUN5C/OUTCAR',structure)
+ #   trajectory = reading.generate_test_trajectory(structure,eigenvectors,original_frequencies,q_vector)
 
     print(structure.get_number_of_cell_atoms())
     print(structure.get_number_of_primitive_atoms())
@@ -72,10 +75,13 @@ if True:
  #   print('trajectory',trajectory.get_energy().shape[0])
     plt.plot(trajectory.get_time().real,trajectory.get_energy().real)
     plt.show()
+    enerfunc.bolzmann_distribution(trajectory.velocity,structure)
+
 
     #Frequency range
-#    test_frequencies_range = np.array([0.01*i + 14.5 for i in range (200)])
-    test_frequencies_range = np.array([0.1*i*1 + 0.1 for i in range (200)])
+    test_frequencies_range = np.array([0.005*i + 14.5 for i in range (400)])
+#    test_frequencies_range = np.array([0.01*i + 3.0 for i in range (200)])
+#    test_frequencies_range = np.array([0.1*i + 0.1 for i in range (200)])
 
 
 ########################################
@@ -94,16 +100,6 @@ plt.suptitle('Trajectory')
 plt.plot(trajectory.get_time().real,trajectory.get_trajectory()[:,1].real)
 plt.show()
 
-
-##################Test s'ha de mirar que coincideixin #######################
-#new_frequencies, new_eigenvectors, dynamical_matrix = eigen.build_dynamical_matrix(trajectory.structure,original_frequencies,eigenvectors)
-
-#print('\n')
-#print('EigenVectors & EigenValues')
-#print(new_frequencies)
-
-#exit()
-#############################################################################
 
 print('Original frequencies')
 print(original_frequencies)