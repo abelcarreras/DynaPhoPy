Implemented text interface Bug correction (related with supercell calculation) Other

Classes/atoms.py
@@ -109,11 +109,16 @@ class Structure:
     #Cell matrix related methods
     def set_primitive_matrix(self,primitive_matrix):
         self._primitive_matrix = primitive_matrix
+        self._number_of_atom_types = None
+        self._number_of_primitive_atoms = None
+        self._atom_type_index = None
+        self._primitive_cell = None
 
 
     def get_primitive_matrix(self):
         if self._primitive_matrix is None:
             if self._primitive_cell is None:
+                print('Warning: No primitive matrix defined! Using unit cell as primitive')
                 self._primitive_matrix = np.identity(self.get_number_of_dimensions())
             else:
                 self._primitive_matrix = np.dot(np.linalg.inv(self.get_cell()),self._primitive_cell)
@@ -132,7 +137,7 @@ class Structure:
         position_super_cell = []
         for k in range(self._positions.shape[0]):
             for r in itertools.product(*[range (i) for i in super_cell]):
-                position_super_cell.append(self._positions[k,:] + np.dot(np.array(r[::-1]),self.get_cell()))
+                position_super_cell.append(self._positions[k,:] + np.dot(np.array(r[::-1]),self.get_cell().T))
         position_super_cell = np.array(position_super_cell)
 
         return position_super_cell
@@ -186,8 +191,15 @@ class Structure:
         return self._cell.shape[0]
 
 
-    def get_atomic_numbers(self):
-        return self._atomic_numbers
+    def get_atomic_numbers(self,super_cell=None):
+        if super_cell is None:
+            super_cell = self.get_number_of_dimensions() * [1]
+
+        atomic_numbers_super_cell = []
+        for j in range(self.get_number_of_cell_atoms()):
+            atomic_numbers_super_cell += [self._atomic_numbers[j] ] * np.prod(super_cell)
+
+        return np.array(atomic_numbers_super_cell,dtype=int)
 
 
     def get_number_of_atoms(self):
@@ -240,8 +252,8 @@ class Structure:
         tolerance = 0.001
         if self._atom_type_index is None:
             primitive_cell_inverse = np.linalg.inv(self.get_primitive_cell())
-            self._atom_type_index = np.array(self.get_number_of_cell_atoms() * [None])
 
+            self._atom_type_index = np.array(self.get_number_of_cell_atoms() * [None])
             counter = 0
             for i in range(self.get_number_of_cell_atoms()):
                 if self._atom_type_index[i] is None:

Classes/controller.py
@@ -20,6 +20,9 @@ class Calculation:
         self._correlation_phonon = None
         self._correlation_wave_vector = None
         self._correlation_direct = None
+        self._band_ranges = None
+        self._bands = None
+
 
 
     #Memory clear methods
@@ -68,7 +71,7 @@ class Calculation:
     def get_eigenvectors(self):
         if self._eigenvectors is None:
             print("Getting frequencies & eigenvectors from Phonopy")
-            self._eigenvectors, self._frequencies = pho_interface.obtain_eigenvectors_from_phonopy(self.dynamic.structure,self.get_reduced_q_vector(),NAC=False)
+            self._eigenvectors, self._frequencies = pho_interface.obtain_eigenvectors_from_phonopy(self.dynamic.structure,self.get_reduced_q_vector(),NAC=True)
             print("Frequencies obtained:")
             print(self._frequencies)
         return self._eigenvectors
@@ -79,6 +82,28 @@ class Calculation:
             print("Getting frequencies & eigenvectors from Phonopy")
             self._eigenvectors, self._frequencies = pho_interface.obtain_eigenvectors_from_phonopy(self.dynamic.structure,self.get_reduced_q_vector())
         return self._frequencies
+
+
+    def set_band_ranges(self,band_ranges):
+        self._band_ranges = band_ranges
+
+
+    def get_band_ranges(self):
+        if self._band_ranges is None:
+            self._band_ranges = [[ [0.0, 0.0, 0.0], [0,1, 0.0, 0.0] ]]
+        return self._band_ranges
+
+
+    def get_phonon_dispersion_spectra(self,band_ranges):
+        if self._bands is None:
+            self._bands = pho_interface.obtain_phonon_dispersion_spectra(self.dynamic.structure,band_ranges,NAC=True)
+
+        for i,freq in enumerate(self._bands[1]):
+            plt.plot(self._bands[1][i],self._bands[2][i],color ='r')
+        plt.axes().get_xaxis().set_visible(False)
+        plt.ylabel('Frequency (THz)')
+        plt.suptitle('Phonon dispersion spectra')
+        plt.show()
 
 
     #Projections related methods
@@ -107,7 +132,7 @@ class Calculation:
     def get_frequency_range(self):
         if self._frequency_range is None:
             print("Not frequency range specified: using default (0-20THz)")
-            self._frequency_range = np.array([0.05*i + 0.1 for i in range (400)])
+            self._frequency_range = np.array([0.05*i + 0.1 for i in range (500)])
         return self._frequency_range
 
 
@@ -134,7 +159,14 @@ class Calculation:
             self._correlation_direct = np.zeros((len(self.get_frequency_range()),self.get_vc().shape[2]))
             for i in range(self.dynamic.get_velocity_mass_average().shape[1]):
                 self._correlation_direct =+ correlate.get_correlation_spectra_par(self.dynamic.get_velocity_mass_average()[:,i,:],self._dynamic,self.get_frequency_range())
+        self._correlation_direct = np.sum(self._correlation_direct,axis=1)
         return self._correlation_direct
+
+
+    def plot_correlation_direct(self):
+        plt.suptitle('Direct correlation')
+        plt.plot(self.get_frequency_range(),self.get_correlation_direct(),'r-')
+        plt.show()
 
 
     def plot_correlation_wave_vector(self):
@@ -145,9 +177,10 @@ class Calculation:
 
     def plot_correlation_phonon(self):
         for i in range(self.get_correlation_phonon().shape[1]):
+            plt.figure(i)
             plt.suptitle('Projection onto Phonon '+str(i+1))
             plt.plot(self.get_frequency_range(),self.get_correlation_phonon()[:,i])
-            plt.show()
+        plt.show()
 
 
     #Printing data to files
@@ -166,3 +199,4 @@ class Calculation:
     #Molecular dynamics analysis related methods
     def show_bolzmann_distribution(self):
         enerfunc.bolzmann_distribution(self.dynamic)
+

Functions/phonopy_interface.py
@@ -84,7 +84,6 @@ def obtain_eigenvectors_from_phonopy(structure,q_vector,NAC=False):
     number_of_dimensions = structure.get_number_of_dimensions()
     number_of_primitive_atoms = structure.get_number_of_primitive_atoms()
 
-#    print(number_of_dimensions,number_of_primitive_atoms)
 
     arranged_ev = np.array([[[eigenvectors [j*number_of_dimensions+k,i]
                                     for k in range(number_of_dimensions)]
@@ -92,6 +91,44 @@ def obtain_eigenvectors_from_phonopy(structure,q_vector,NAC=False):
                                     for i in range(number_of_primitive_atoms*number_of_dimensions)])
 
     return arranged_ev, frequencies
+
+def obtain_phonon_dispersion_spectra(structure,bands_ranges,NAC=False):
+
+    print('Calculating phonon dispersion spectra...')
+    bulk = PhonopyAtoms(symbols=structure.get_atomic_types(),
+                        scaled_positions=structure.get_scaled_positions())
+    bulk.set_cell(structure.get_cell())
+
+    phonon = Phonopy(bulk,structure.get_super_cell_phonon(),
+                     primitive_matrix= structure.get_primitive_matrix(),
+                     is_auto_displacements=False)
+
+    if NAC:
+        print("Phonopy warning: Using Non Analitical Corrections")
+        get_is_symmetry = True  #sfrom phonopy:   settings.get_is_symmetry()
+        primitive = phonon.get_primitive()
+        nac_params = parse_BORN(primitive, get_is_symmetry)
+        phonon.set_nac_params(nac_params=nac_params)
+
+
+    phonon.set_displacement_dataset(copy.deepcopy(structure.get_force_set()))
+    phonon.produce_force_constants()
+
+
+    band_resolution =30
+    bands =[]
+    for q_start, q_end in bands_ranges:
+        band = []
+        for i in range(band_resolution+1):
+            band.append(np.array(q_start) +
+                        (np.array(q_end) - np.array(q_start)) / band_resolution * i)
+        bands.append(band)
+
+
+    phonon.set_band_structure(bands)
+    return phonon.get_band_structure()
+
+#if __name__ == 'phonopy_interface.py'
 
 '''
 #Starting test program

Functions/reading.py
@@ -4,8 +4,15 @@ import Classes.atoms as atomtest
 import mmap
 import pickle
 import Functions.phonopy_interface as pho_interface
+import os
 
 def read_from_file_structure(file_name):
+
+    #Check file exists
+    if not os.path.isfile(file_name):
+        print('Structure file does not exist!')
+        exit()
+
     #Read from VASP OUTCAR file
     print("Reading VASP structure")
 
@@ -17,21 +24,22 @@ def read_from_file_structure(file_name):
         #Setting number of dimensions
         number_of_dimensions = 3
 
+        #Test reading for guessing primitive cell (Not stable)
+        if False:
+           #Reading primitive cell (not sure about this, by default disabled)
+            position_number = file_map.find('PRICEL')
+            file_map.seek(position_number)
+            position_number = file_map.find('A1')
+            file_map.seek(position_number)
 
-       #Reading primitive cell (not sure about this, by default disabled)
-        position_number = file_map.find('PRICEL')
-        file_map.seek(position_number)
-        position_number = file_map.find('A1')
-        file_map.seek(position_number)
-
-        primitive_cell = []    #Primitive Cell
-        for i in range (number_of_dimensions):
-            primitive_cell.append(file_map.readline()
-                                      .replace(",", "")
-                                      .replace(")", "")
-                                      .replace(")","")
-                                      .split()[3:number_of_dimensions+3])
-        primitive_cell = np.array(primitive_cell,dtype="double").T
+            primitive_cell = []    #Primitive Cell
+            for i in range (number_of_dimensions):
+                primitive_cell.append(file_map.readline()
+                                          .replace(",", "")
+                                          .replace(")", "")
+                                          .replace(")","")
+                                          .split()[3:number_of_dimensions+3])
+            primitive_cell = np.array(primitive_cell,dtype="double").T
 
 
         #Reading number of atoms
@@ -105,19 +113,24 @@ def read_from_file_structure(file_name):
                               )
 
 
-def read_from_file_trajectory(file_name,structure=None):
+def read_from_file_trajectory(file_name,structure=None,
+                              limit_number_steps=100000,  #Maximum number of steps read
+                              last_steps=1000):           #Total number of read steps
 
+    #Check file exists
+    if not os.path.isfile(file_name):
+        print('Trajectory file does not exist!')
+        exit()
+
+    #Starting reading
     print("Reading VASP trajectory")
+    print("This could take long, please wait..")
 
     #Dimensionality of VASP calculation
     number_of_dimensions = 3
 
-    #Maximum number of structures that's gonna be read
-    limit_number_structures = 100000
-    last_points_taken = 60000
-
     with open(file_name, "r+") as f:
-    # memory-map the file
+        #Memory-map the file
         file_map = mmap.mmap(f.fileno(), 0)
         position_number=file_map.find('NIONS =')
         file_map.seek(position_number+7)
@@ -140,13 +153,11 @@ def read_from_file_trajectory(file_name,structure=None):
         file_map.seek(position_number)
         file_map.readline()
 
-
     # Check if number of atoms is multiple of cell atoms
         if structure:
             if number_of_atoms % structure.get_number_of_cell_atoms() != 0:
                 print('Warning: Number of atoms not matching, check VASP output files')
             structure.set_number_of_atoms(number_of_atoms)
-
 
 #       Read coordinates and energy
         trajectory = []
@@ -167,21 +178,19 @@ def read_from_file_trajectory(file_name,structure=None):
             read_energy = file_map.readline().split()[2]
             trajectory.append(np.array(read_coordinates,dtype=float).flatten()) #in angstrom
             energy.append(np.array(read_energy,dtype=float))
-            limit_number_structures -= 1
+            limit_number_steps -= 1
 
-            if limit_number_structures < 0:
+            if limit_number_steps < 0:
                 break
 
         file_map.close()
 
         trajectory = np.array([[[trajectory[i][j*number_of_dimensions+k] for k in range(number_of_dimensions) ] for j in range(number_of_atoms)] for i in range (len(trajectory))])
+        trajectory = trajectory[-last_steps:,:,:]
+        energy = energy[-last_steps:]
 
-        trajectory = trajectory[-last_points_taken:,:,:]
-        energy = energy[-last_points_taken:]
-
-        print('Number of total read steps:',trajectory.shape[0])
+        print('Number of total steps read:',trajectory.shape[0])
         time = np.array([ i*time_step for i in range(trajectory.shape[0])],dtype=float)
-
 
         print('Trajectory file read')
         return dyn.Dynamics(structure = structure,
@@ -191,7 +200,8 @@ def read_from_file_trajectory(file_name,structure=None):
                             super_cell=super_cell)
 
 
-def generate_test_trajectory(structure,q_vector_o):
+#Just for testing
+def generate_test_trajectory(structure,q_vector_o,super_cell=None):
 
     print('Making fake ideal data for testing')
     if False:
@@ -200,7 +210,10 @@ def generate_test_trajectory(structure,q_vector_o):
         return  trajectory
 
 
-    super_cell= structure.get_super_cell_matrix()
+#    super_cell= structure.get_super_cell_matrix()
+    if super_cell is None:
+        super_cell = [1,1,1]
+
     print(q_vector_o)
 #    q_vector_o = np.array ([0.25,0.25,0.25])
 #    q_vector_o = np.prod([[0.25,0.25,0.25],2*np.pi/structure.get_primitive_cell().diagonal()],axis=0)
@@ -209,9 +222,9 @@ def generate_test_trajectory(structure,q_vector_o):
     positions = structure.get_positions(super_cell=super_cell)
     masses = structure.get_masses(super_cell=super_cell)
 
-    total_time = 2.0
+    total_time = 0.1
     time_step = 0.001
-    amplitude = 1.0
+    amplitude = 0.0
 #    print('Freq Num',number_of_frequencies)
 
     for i in range(structure.get_number_of_dimensions()):
@@ -236,6 +249,7 @@ def generate_test_trajectory(structure,q_vector_o):
     eigenvectors_r = []
     frequencies_r = []
     for i in range(len(q_vector_r)):
+        print(q_vector_r[i])
         eigenvectors, frequencies = pho_interface.obtain_eigenvectors_from_phonopy(structure,q_vector_r[i])
         eigenvectors_r.append(eigenvectors)
         frequencies_r.append(frequencies)
@@ -250,10 +264,12 @@ def generate_test_trajectory(structure,q_vector_o):
         xyz_file.write(str(number_of_atoms) + '\n\n')
         coordinates = []
         for i_atom in range(number_of_atoms):
-            coordinate = map(complex,positions[i_atom,:])
+           # coordinate = map(complex,positions[i_atom,:])
+            coordinate = np.array(positions[i_atom,:],dtype=complex)
             for i_freq in range(number_of_frequencies):
                 for i_long in range(q_vector_r.shape[0]):
-                    q_vector = np.prod([q_vector_r[i_long,:],2*np.pi/structure.get_primitive_cell().diagonal()],axis=0)
+                    q_vector = np.dot(q_vector_r[i_long,:], 2*np.pi*np.linalg.inv(structure.get_primitive_cell()).T)
+              #      q_vector = np.prod([q_vector_r[i_long,:],2*np.pi/structure.get_primitive_cell().diagonal()],axis=0)
                     # Beware in the testing amplitude!! Made for all phonons have the same!!
                     coordinate += amplitude / (np.sqrt(masses[i_atom]) *frequencies_r[i_long][i_freq])*\
                                   eigenvectors_r[i_long][i_freq,atom_type[i_atom]]*\
@@ -291,9 +307,7 @@ def generate_test_trajectory(structure,q_vector_o):
                         energy = np.array(energy),
                         time=time)
 
-
-
-
+#Just for testing
 def read_from_file_test():
 
     print('Reading structure from test file')
@@ -364,7 +378,6 @@ def read_from_file_test():
                         structure=structure)
 
 
-
 def write_correlation_to_file(frequency_range,correlation_vector,file_name):
     output_file = open(file_name, 'w')
 
@@ -377,7 +390,54 @@ def write_correlation_to_file(frequency_range,correlation_vector,file_name):
     output_file.close()
     return 0
 
-#print (read_from_file_structure('../Data Files/NaCl/OUTCAR').get_number_of_dimensions())
-#print (read_from_file_structure2('../Data Files/NaCl/OUTCAR'))
-#print (read_from_file_trajectory('/home/abel/Desktop/Bi2O3_md/OUTCAR')[1,79,:])
 
+def read_parameters_from_input_file(file_name):
+
+    primitive_matrix = None
+    super_cell_matrix = None
+    structure_file_name = None
+    force_constants_file_name = None
+    bands = None
+
+    input_file = open(file_name , "r").readlines()
+    for i,line in enumerate(input_file):
+
+        if "STRUCTURE FILE" in line:
+
+            structure_file_name = input_file[i+1].replace('\n','')
+
+        if "FORCE CONSTANTS" in line:
+            force_constants_file_name = input_file[i+1].replace('\n','')
+
+
+        if "PRIMITIVE MATRIX" in line:
+            primitive_matrix = [input_file[i+1].replace('\n','').split(),
+                                input_file[i+2].replace('\n','').split(),
+                                input_file[i+3].replace('\n','').split()]
+            primitive_matrix = np.array(primitive_matrix,dtype=float)
+
+        if "SUPERCELL MATRIX PHONOPY" in line:
+            super_cell_matrix = [input_file[i+1].replace('\n','').split(),
+                                 input_file[i+2].replace('\n','').split(),
+                                 input_file[i+3].replace('\n','').split()]
+
+            super_cell_matrix = np.array(super_cell_matrix,dtype=int)
+
+        if "BANDS" in line:
+            bands = []
+            while i < len(input_file)-1:
+                try:
+                    band =  np.array(input_file[i+1].replace(',',' ').split(),dtype=float).reshape((2,3))
+                except IOError:
+                    break
+                except ValueError:
+                    break
+                i += 1
+                bands.append(band)
+
+
+    return  { 'structure_file_name':structure_file_name,
+              'force_constants_file_name':force_constants_file_name,
+              'primitive_matrix':primitive_matrix,
+              'super_cell_matrix':super_cell_matrix,
+              'bands':bands}

input.py
@@ -1,8 +1,10 @@
-import numpy as np
+#!/usr/bin/env python
+
+#import numpy as np
 import Functions.reading as reading
 import phonopy.file_IO as file_IO
 import Classes.controller as controller
-
+import Functions.phonopy_interface as pho_interface
 
 ##################################  STRUCTURE FILES #######################################
 # 1. Set the directory in where the FORCE_SETS and structure OUTCAR are placed
@@ -10,12 +12,11 @@ import Classes.controller as controller
 # OUTCAR : Single Point calculation of the unit cell structure used in PHONOPY calculation
 
 #directory ='/home/abel/VASP/Si-phonon/4x4x4B/'
-directory = '/home/abel/VASP/MgO-phonon/3x3x3/'
+#directory = '/home/abel/VASP/MgO-phonon/3x3x3/'
+directory = '/home/abel/VASP/GaN-phonon/3x3x3/'
 
 structure = reading.read_from_file_structure(directory+'OUTCAR')
 structure.set_force_set(file_IO.parse_FORCE_SETS(filename=directory+'FORCE_SETS'))
-
-
 
 
 ############################### PHONOPY CELL INFORMATION ####################################
@@ -31,6 +32,10 @@ structure.set_primitive_matrix([[0.0, 0.5, 0.5],
                                 [0.5, 0.0, 0.5],
                                 [0.5, 0.5, 0.0]])
 
+structure.set_primitive_matrix([[1.0, 0.0, 0.0],
+                                [0.0, 1.0, 0.0],
+                                [0.0, 0.0, 1.0]])
+
 # 3. Set super cell phonon, this matrix denotes the super cell used in PHONOPY for creating
 # the finite displacements
 
@@ -38,14 +43,26 @@ structure.set_super_cell_phonon([[3, 0, 0],
                                  [0, 3, 0],
                                  [0, 0, 3]])
 
+
+#Checking values
+#print(structure.get_atom_type_index(super_cell=[1,1,1]))
+#print(structure.get_atomic_numbers(super_cell=[1,1,1]))
+#print(structure.get_atomic_types())
+#print(structure.get_masses(super_cell=[1,1,1]))
+#print(structure.get_number_of_atom_types())
+
+
 ################################### TRAJECTORY FILES ##########################################
 # 4. Set the location of OUTCAR file containing the Molecular Dynamics trajectory
 
 #trajectory = reading.read_from_file_trajectory('/home/abel/VASP/Si-dynamic_300/RUN1/OUTCAR',structure)
 trajectory = reading.read_from_file_trajectory('/home/abel/VASP/MgO-dynamic_300/RUN3/OUTCAR',structure)
+
+eigenvectors, frequencies = pho_interface.obtain_eigenvectors_from_phonopy(structure,[0,0,0],NAC=True)
+
+trajectory = reading.generate_test_trajectory(structure,[0.0, 0.0, 0.0],super_cell=[4,4,2])
+
 calculation = controller.Calculation(trajectory)
-
-
 
 
 ############################## DEFINE CALCULATION REQUESTS #####################################
@@ -55,28 +72,28 @@ calculation = controller.Calculation(trajectory)
 # Python scripting features may be used for more complex requests
 
 # 5a. Set wave vector into which is going to be projected the velocity (default: gamma point)
-calculation.set_reduced_q_vector([0.5, 0.5, 0.5])
+calculation.set_reduced_q_vector([0.5, 0.0, 0.0])
 
-# 5b. Define range of frequency to analize (default: 0-20THz)
+# 5b. Define range of frequency to analyze (default: 0-20THz)
 #calculation.set_frequency_range(np.array([0.01*i + 14.0 for i in range (100)]))
 
 # 5c. Request Boltzmann distribution trajectory analysis
-calculation.show_bolzmann_distribution()
+#calculation.show_bolzmann_distribution()
 
 # 5d. Request calculate plot of direct velocity correlation function (without projection)
 #calculation.plot_correlation_direct()
 
 # 5e. Request calculate plot of wave vector projected velocity correlation function
-#calculation.plot_correlation_wave_vector()
+calculation.plot_correlation_wave_vector()
 
 # 5f. Request calculate plot of phonon mode projected velocity correlation function
-calculation.plot_correlation_phonon()
+#calculation.plot_correlation_phonon()
 
 # 5g. Request save direct velocity correlation function into file
 #calculation.write_correlation_direct('Data Files/correlation_d.out')
 
 # 5h. Request save wave vector projected velocity correlation function into file
-#calculation.write_correlation_wave_vector('Data Files/correlation_w2.out')
+calculation.write_correlation_wave_vector('Data Files/correlation_w.out')
 
 # 5i. Request save phonon projected velocity correlation function into file
 calculation.write_correlation_phonon('Data Files/correlation_p.out')

text_ui.py
@@ -25,12 +25,25 @@ for i,line in enumerate(input_file):
 
     if "PRIMITIVE MATRIX" in line:
         print('Primitive')
+        primitive_matrix = [input_file[i+1].replace('\n','').split(),
+                            input_file[i+2].replace('\n','').split(),
+                            input_file[i+3].replace('\n','').split()]
 
+        primitive_matrix = np.array(primitive_matrix,dtype=float)
+
+    if "SUPERCELL MATRIX PHONOPY" in line:
+        print('Super cell')
+        super_cell_matrix = [input_file[i+1].replace('\n','').split(),
+                             input_file[i+2].replace('\n','').split(),
+                             input_file[i+3].replace('\n','').split()]
+
+        super_cell_matrix = np.array(super_cell_matrix,dtype=int)
 
 
 print(structure_file_name)
 print(force_constants_file_name)
-
+print(primitive_matrix)
+print(super_cell_matrix)
 
 
 