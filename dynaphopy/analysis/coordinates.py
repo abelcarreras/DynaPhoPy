Fixing velocity calculation

dynaphopy/analysis/coordinates.py
@@ -37,19 +37,14 @@ def relativize_trajectory(dynamic):
 
 
     for j in range(number_of_atoms):
-
-    #    print(trajectory, position, cell)
-    #    print (disp.relative_trajectory(cell, trajectory[:,j,:], position[j]))
         for i in range(0, normalized_trajectory.shape[0]):
-            difference = trajectory[i, j, :] - position[j]
-            difference_matrix = np.dot(np.linalg.inv(cell), difference)
-#            print(difference_matrix)
 
-        normalized_trajectory[:, j, :] = disp.relative_trajectory(cell, trajectory[:, j, :], position[j])
+            difference = normalized_trajectory[i, j, :] - position[j]
+
+            difference_matrix = np.around(np.dot(np.linalg.inv(cell), difference), decimals=0)
+            normalized_trajectory[i, j, :] -= np.dot(difference_matrix, cell) + position[j]
 
         progress_bar(float(j+1)/number_of_atoms)
-
-  #  print(normalized_trajectory)
 
     return normalized_trajectory
 
@@ -70,12 +65,10 @@ def relativize_trajectory_py(dynamic):
 
          #   difference_matrix = np.array(np.dot(np.linalg.inv(cell),(IniSep)),dtype=int)
             difference_matrix = np.around(np.dot(np.linalg.inv(cell), difference), decimals=0)
-
             normalized_trajectory[i, j, :] -= np.dot(difference_matrix, cell) + position[j]
 
         progress_bar(float(j+1)/number_of_atoms)
 
-  #  print(normalized_trajectory)
     return normalized_trajectory
 
 def trajectory_projection(dynamic, direction):

dynaphopy/classes/dynamics.py
@@ -5,15 +5,6 @@ from dynaphopy.derivative import derivative as derivative
 from dynaphopy.analysis.coordinates import relativize_trajectory_py as relativize_trajectory
 
 
-def obtain_velocity_from_positions(cell, trajectory, time):
-    velocity = np.empty_like(trajectory)
-    for i in range(trajectory.shape[1]):
-        velocity[:, i, :] = derivative(cell, trajectory[:, i, :], time)
-     # velocity[:, i, :] = derivative(cell, trajectory[:, i, :], time, precision_order=6)
-
-    print('Velocity obtained from trajectory derivative')
-    return velocity
-
 class Dynamics:
 
     def __init__(self,
@@ -156,8 +147,10 @@ class Dynamics:
     def velocity(self):
         if self._velocity is None:
             print('No velocity provided! calculating it from coordinates...')
-            self._velocity = obtain_velocity_from_positions(self.get_super_cell(),self.trajectory,self.get_time_step_average())
- #           self._velocity = obtain_velocity_from_positions(self.get_super_cell(),self.trajectory,self.get_time())
+            self._velocity = np.zeros_like(self.get_relative_trajectory(), dtype=complex)
+            for i in range(self.get_number_of_atoms()):
+                for j in range(self.structure.get_number_of_dimensions()):
+                    self._velocity[:,i,j] = np.gradient(self.get_relative_trajectory()[:,i,j],self.get_time_step_average())
 
         return self._velocity
 