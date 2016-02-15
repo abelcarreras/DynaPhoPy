fix bug with equivalent q-points calculation.  affecting hexgonal systems

dynaphopy/interface/phonopy_link.py
@@ -142,13 +142,16 @@ def get_equivalent_q_points_by_symmetry(q_point, structure):
 
     tot_points = []
     for operation_matrix in Symmetry(bulk).get_reciprocal_operations():
-        operation_matrix_q = np.dot(np.linalg.inv(structure.get_primitive_matrix().T), operation_matrix)
+        operation_matrix_q = np.dot(np.linalg.inv(structure.get_primitive_matrix()), operation_matrix.T)
         operation_matrix_q = np.dot(operation_matrix_q, structure.get_primitive_matrix().T)
 
         q_point_test = np.dot(q_point, operation_matrix_q)
 
         if (q_point_test >= 0).all():
                 tot_points.append(q_point_test)
+
+#    print tot_points
+#    print(np.vstack({tuple(row) for row in tot_points}))
 
     return np.vstack({tuple(row) for row in tot_points})
 