fixed thermal properties calculation

dynaphopy/__init__.py
@@ -707,13 +707,14 @@ class Calculation:
 
 
         phonopy_dos = pho_interface.obtain_phonopy_dos(self.dynamic.structure,
+                                                       freq_min=0.01,
+                                                       freq_max=self.get_frequency_range()[-1],
                                                        mesh=self.parameters.mesh_phonopy)
-
 
         phonopy_dos_r = pho_interface.obtain_phonopy_dos(self.dynamic.structure,
                                                          mesh=self.parameters.mesh_phonopy,
                                                          freq_min=0.01,
-                                      #                   freq_max=self.get_frequency_range()[-1],
+                                                         freq_max=self.get_frequency_range()[-1],
                                                          force_constants=self._renormalized_force_constants)
 
         self.set_frequency_limits([0, np.max(phonopy_dos[0])*1.2])
@@ -729,13 +730,13 @@ class Calculation:
 
         # Renormalized force constants
         free_energy = thm.get_free_energy(temperature, phonopy_dos_r[0], phonopy_dos_r[1]) + \
-                      thm.get_free_energy_correction_aprox(temperature, phonopy_dos[0], phonopy_dos_r[1], phonopy_dos[1])
+                      thm.get_free_energy_correction_2(temperature, phonopy_dos[0], phonopy_dos_r[1], phonopy_dos[1])
         entropy = thm.get_entropy(temperature, phonopy_dos_r[0], phonopy_dos_r[1])
         c_v = thm.get_cv(temperature, phonopy_dos_r[0], phonopy_dos_r[1])
         integration = np.trapz(phonopy_dos_r[1], x=phonopy_dos_r[0])/(self.dynamic.structure.get_number_of_atoms()*
                                                        self.dynamic.structure.get_number_of_dimensions())
         renormalized_properties = [free_energy, entropy, c_v, integration]
-        print('Free energy correction: {0:12.4f} KJ/mol'.format(thm.get_free_energy_correction_aprox(temperature, phonopy_dos[0], phonopy_dos_r[1], phonopy_dos[1])))
+        print('Free energy correction: {0:12.4f} KJ/mol'.format(thm.get_free_energy_correction_2(temperature, phonopy_dos[0], phonopy_dos_r[1], phonopy_dos[1])))
 
         if from_power_spectrum:
             normalization = np.prod(self.dynamic.get_super_cell_matrix())

dynaphopy/analysis/thermal_properties.py
@@ -46,7 +46,7 @@ def get_free_energy_correction(temperature, frequency, dos, shift):
     return free_energy_c
 
 
-def get_free_energy_correction_aprox(temperature, frequency, dos, dos_r):
+def get_free_energy_correction_2(temperature, frequency, dos, dos_r):
 
     def n(temp, freq):
         return pow(np.exp(freq*h_bar/(k_b*temp))-1, -1)
@@ -141,9 +141,9 @@ if __name__ == "__main__":
     #free_energy = get_free_energy(temp,frequency,dos) + get_free_energy_correction(temp, frequency, dos, shift)
 
     print (get_free_energy_correction(temp, frequency, dos, shift),
-           get_free_energy_correction_aprox(temp, frequency, dos, dos_r))
+           get_free_energy_correction_2(temp, frequency, dos, dos_r))
 
-    free_energy = get_free_energy(temp, frequency_r, dos_r) + get_free_energy_correction_aprox(temp, frequency, dos_r, dos)
+    free_energy = get_free_energy(temp, frequency_r, dos_r) + get_free_energy_correction_2(temp, frequency, dos_r, dos)
     entropy = get_entropy(temp, frequency_r, dos_r)
     c_v = get_cv(temp, frequency_r, dos_r)
     print ('Renormalized')