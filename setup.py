62c2bd3492c1048cc9f70f0f361de199b1954943
abel <abelcarreras83@gmail.com>
2016-04-28 11:21:24 +0900

    update README file and version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ displacements = Extension('dynaphopy.displacements',
 
 
 setup(name='dynaphopy',
-      version='1.10',
+      version='1.11',
       description='dynaphopy module',
       author='Abel Carreras',
       url='https://github.com/abelcarreras/DynaPhoPy',
