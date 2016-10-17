Fixed the calculation of damped oscillator function area. Corrected area of asymmetric lorentzian function

dynaphopy/__init__.py
@@ -335,7 +335,7 @@ class Calculation:
         np.savetxt(file_name, self.get_vq().real)
 
     #Power spectra related methods
-    def select_power_spectra_algorithm(self,algorithm):
+    def select_power_spectra_algorithm(self, algorithm):
         if algorithm in power_spectrum_functions.keys():
             if algorithm != self.parameters.power_spectra_algorithm:
                 self.power_spectra_clear()
@@ -346,6 +346,18 @@ class Calculation:
             for i in power_spectrum_functions.keys():
                 print('{0} : {1}'.format(i,power_spectrum_functions[i][1]))
             exit()
+
+    def select_fitting_function(self, function):
+        from dynaphopy.analysis.fitting.fitting_functions import Fitting_functions
+        if function in Fitting_functions.keys():
+            if function != self.parameters.fitting_function:
+                self.parameters.fitting_function = function
+        else:
+            print("Fitting function number not found!\nPlease select:")
+            for i in Fitting_functions.keys():
+                print('{0} : {1}'.format(i, Fitting_functions[i]))
+            exit()
+
 
     def get_power_spectrum_phonon(self):
         if self._power_spectrum_phonon is None:

dynaphopy/analysis/fitting/fitting_functions.py
@@ -60,6 +60,8 @@ class Lorentzian():
             width = 2.0*fit_params[1]
             frequency = fit_params[0]
             area = fit_params[2]/(2 * np.pi)
+
+
             error = get_error_from_covariance(fit_covariances)
             base_line = fit_params[3]
 
@@ -120,6 +122,7 @@ class Lorentzian_asymmetric():
 
 
     def get_fitting(self):
+        from scipy.integrate import quad
 
         try:
 
@@ -146,7 +149,11 @@ class Lorentzian_asymmetric():
             width = self._g_a(frequency, fit_params[0], fit_params[1], fit_params[4])*2
             asymmetry = fit_params[4]
 
-            area = fit_params[2]/(2 * np.pi)
+            area, error_integration = quad(self._function, 0, self.test_frequencies_range[-1],
+                                           args=tuple(fit_params),
+                                           epsabs=1e-8)
+            area /=2*np.pi
+        #    area = fit_params[2]/(2 * np.pi)
             error = get_error_from_covariance(fit_covariances)
             base_line = fit_params[3]
 
@@ -195,6 +202,7 @@ class Damped_harmonic():
         return c/(((a**2-x**2)**2 + (b*x)**2))+d
 
     def get_fitting(self):
+        from scipy.integrate import quad
 
         try:
 
@@ -214,9 +222,17 @@ class Damped_harmonic():
             width = abs(fit_params[1])
             maximum = fit_params[2]/(width*np.pi)
             frequency = fit_params[0]
-            area = fit_params[2]*np.pi/(fit_params[0]**3*width)
+
+            area, error_integration = quad(self._function, 0, self.test_frequencies_range[-1],
+                                           args=tuple(fit_params),
+                                           epsabs=1e-8)
+            area /=2*np.pi
+#            area = fit_params[2]*np.pi/(fit_params[0]**3*width)
+
+
             error = get_error_from_covariance(fit_covariances)
             base_line = fit_params[3]
+
 
 
             return {'maximum': maximum,

dynaphopy/power_spectrum.py
@@ -82,7 +82,7 @@ def get_mem_spectra_par_openmp(vq, trajectory, parameters):
 
 # Coefficient analysis for MEM
 def mem_coefficient_scan_analysis(vq, trajectory, parameters):
-    import dynaphopy.analysis.fitting.fitting_functions
+    from dynaphopy.analysis.fitting import fitting_functions
 
     mem_full_dict = {}
 
@@ -106,11 +106,11 @@ def mem_coefficient_scan_analysis(vq, trajectory, parameters):
             guess_height = np.max(power_spectrum)
             guess_position = test_frequency_range[np.argmax(power_spectrum)]
 
-            Fitting_function_class = fitting_functions.Fitting_functions[fitting_function_type]
+            Fitting_curve = fitting_functions.Fitting_functions[parameters.fitting_function_type]
             fitting_function = Fitting_curve(test_frequency_range,
-                                                            power_spectrum,
-                                                            guess_height=guess_height,
-                                                            guess_position=guess_position)
+                                             power_spectrum,
+                                             guess_height=guess_height,
+                                             guess_position=guess_position)
 
             fitting_parameters = fitting_function.get_fitting()
 
@@ -181,7 +181,7 @@ def mem_coefficient_scan_analysis(vq, trajectory, parameters):
         ax3.plot(test_frequency_range, mem_full_dict[i][0], label='Power spectrum')
         ax3.plot(test_frequency_range,
                  fitting_function._function(test_frequency_range, *scan_params[best_index]),
-                 label='Lorentzian fit')
+                 label='{} fit'.format(fitting_function.curve_name))
 
         plt.show()
 