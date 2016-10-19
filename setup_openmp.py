7c21ddb4fff40cb232138c6cd07cedccccc25465
abel <abelcarreras83@gmail.com>
2016-10-19 14:13:30 +0900

    add power_spectrum module to setup.py

--- setup_openmp.py
+++ setup_openmp.py
@@ -30,6 +30,7 @@ setup(name='dynaphopy',
       author_email='abelcarreras83@gmail.com',
       packages=['dynaphopy',
                 'dynaphopy.orm',
+                'dynaphopy.power_spectrum'
                 'dynaphopy.analysis',
                 'dynaphopy.interface'],
       scripts=['scripts/dynaphopy'],
