30c44e56f30ce84ecd09e91cba432e106bdb091d
abel <abelcarreras83@gmail.com>
2017-02-10 12:53:25 +0900

    Fix bug in np.unique function (to force not sorting string list)  in  generating supercell using  c_poscar/c_lammps option.

--- dynaphopy/generate_cell.py
+++ dynaphopy/generate_cell.py
@@ -8,7 +8,8 @@ def generate_VASP_structure(structure, scaled=False, supercell=(1, 1, 1)):
     types = structure.get_atomic_types(supercell=supercell)
 
     atom_type_unique = np.unique(types, return_index=True)
-    # To prevent an unexpected behavior of unique
+
+    # To use unique without sorting
     sort_index = np.argsort(atom_type_unique[1])
     elements = np.array(atom_type_unique[0])[sort_index]
     elements_count= np.diff(np.append(np.array(atom_type_unique[1])[sort_index], [len(types)]))
@@ -45,7 +46,7 @@ def generate_LAMMPS_structure(structure, supercell=(1, 1, 1), by_element=True):
 
         type_index_unique = np.unique(types, return_index=True)[1]
 
-        # To prevent an unexpected behavior of unique
+        # To use unique without sorting
         sort_index = np.argsort(type_index_unique)
         type_index_unique = np.array(type_index_unique)[sort_index]
 
