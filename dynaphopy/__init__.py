c117802509e9ac0d46d610395f1399675013ee46
Abel Carreras <abelcarreras83@gmail.com>
2024-07-23 12:53:51 +0200

    bump version

--- dynaphopy/__init__.py
+++ dynaphopy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.17.15'
+__version__ = '1.17.16'
 import dynaphopy.projection as projection
 import dynaphopy.parameters as parameters
 import dynaphopy.interface.phonopy_link as pho_interface
